aea8654610ad23adb1f5bc86c042b5019e3a8fb3
Franco Direnzo <fm.direnzo@gmail.com>
2024-05-29 12:35:18 -0300

    completed ejercicio4

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,1 +1,6 @@
-console.log("test");
+document.getElementById("clickMe").onclick = test;
+function test(){
+    var h=document.getElementById("h").value;
+    var m=document.getElementById("m").value;
+    alert(m/(h*0.01*h*0.01));
+}   
